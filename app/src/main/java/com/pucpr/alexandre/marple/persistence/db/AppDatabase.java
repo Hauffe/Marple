092d972f73b5054e1877bc43ceca940b43c906da
Alexandre <alex_hauffe@hotmail.com>
2019-06-10 23:29:59 -0300

    Start!

--- app/src/main/java/com/pucpr/alexandre/marple/persistence/db/AppDatabase.java
+++ app/src/main/java/com/pucpr/alexandre/marple/persistence/db/AppDatabase.java
@@ -9,7 +9,7 @@ import com.pucpr.alexandre.marple.persistence.IngredientDAO;
 import com.pucpr.alexandre.marple.entity.Ingredient;
 import com.pucpr.alexandre.marple.utils.Constants;
 
-@Database(entities = {Ingredient.class}, version = 1)
+@Database(entities = {Ingredient.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract IngredientDAO ingredientDAO();
